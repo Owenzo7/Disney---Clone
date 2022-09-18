502e83dce660232d25f5dba9142d83ca626f0c26
Owen Lee <owenlee545@gmail.com>
2022-09-18 13:09:06 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+  return (
+    <Nav>
+        Header
+        </Nav>
+  )
+}
+
+export default Header
+
+
+const Nav = styled.div`
+
+height:70px;
+background: #090b13;
+
+
+`

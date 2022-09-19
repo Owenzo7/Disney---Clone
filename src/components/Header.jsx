5ed0fafaf37b046394b7bb560f51921181aa7a63
Owen Lee <owenlee545@gmail.com>
2022-09-19 20:24:34 +0300

    edited my header component with some a tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,22 @@ import styled from 'styled-components'
 function Header() {
   return (
     <Nav>
-        <Logo src="" />
+        <Logo src="/images/logo.svg" />
 
           <NavMenu>
 
+            <a>
+            <img src="/images/home-icon.svg" alt="" />
+            <span>HOME</span>
+            </a>
+            <a>
+            <img src="/images/search-icon.svg" alt="" />
+            <span>SEARCH</span>
+            </a>
+            <a>
+            <img src="/images/search-icon.svg" alt="" />
+            <span>SEARCH</span>
+            </a>
 
           </NavMenu>
         </Nav>
@@ -20,6 +32,9 @@ export default Header
 const Nav = styled.nav`
 height: 70px;
 background: #090b13;
+display: flex;
+align-items: center;
+padding: 0 36px;
 
 `
 
@@ -29,6 +44,23 @@ width:80px;
 `
 
 const NavMenu = styled.div`
+display: flex;
 
+a{
 
+  display: flex;
+  align-items: center;
+  padding: 0 12px;
+
+  img{
+
+    height:20px;
+  }
+
+  span{
+
+    font-size: 13px;
+    letter-spacing:1.4 2px; 
+  }
+}
 `

489be3f08b90cad03c2901daca499e09e5a40334
Owen Lee <owenlee545@gmail.com>
2022-09-20 18:52:01 +0300

    Added the Detail component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Viewers from './Viewers'
 import Movies from './Movies'
+import Detail from './Detail'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
         <ImgSlider />
         <Viewers   /> 
         <Movies    />
+        <Detail    />
 
     </Container>
   )

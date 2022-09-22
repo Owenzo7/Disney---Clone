d36b5ada3c112e24bcf43abc40af9d13534f5884
Owen Lee <owenlee545@gmail.com>
2022-09-22 14:11:58 +0300

    implemented the Login component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Counter } from "./features/counter/Counter";
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import Login from './components/Login';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import Detail from './components/Detail';
+
 
 
 function App() {
@@ -18,6 +20,9 @@ function App() {
       <Router>
       <Header />
       <Switch>
+        <Route path = "/login" >
+           <Login />
+        </Route>
         <Route path = "/detail" >
           <Detail />
         </Route>
